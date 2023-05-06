e299031e678e419994f8daa181d6e99eb8d4dfbb
kim0chan <39790536+kim0chan@users.noreply.github.com>
2023-05-06 18:14:40 +0900

    added component(star)

--- Assets/Scripts/Spawner.cs
+++ Assets/Scripts/Spawner.cs
@@ -4,14 +4,24 @@ using UnityEngine;
 
 public class Spawner : MonoBehaviour
 {
+    //Obstacle Section
     public GameObject[] obstacle;
     int lr = 1;
     public float interval = 3.0f;
     public float endgame_interval = 1.0f;
     public float obstacleSpeed = 12.0f;
     public float endgame_obstacleSpeed = 24.0f;
+    public float spawn_point;
+    public float prev_spawn_point;
     float pivot = 7.0f;
     float timer = 0.0f;
+
+    // Star Section
+    public GameObject[] star;
+    public float Star_Prob = 9.0f;
+    public int Star_interval = 5;
+    public int Star_timer = 0;
+
     // Start is called before the first frame update
     void Start()
     {
@@ -23,13 +33,28 @@ public class Spawner : MonoBehaviour
     {
         if(timer >= interval)
         {
-            float spawn_point = pivot + Random.Range(-pivot + 3.0f - interval, 4.0f);
-            GameObject newObstacle = Instantiate(obstacle[(lr + 1) / 2], new Vector3(spawn_point * lr, 0.5f, -40), this.transform.rotation);
+            spawn_point = pivot + Random.Range(-pivot + 3.0f - interval, 4.0f);
+            prev_spawn_point = spawn_point * lr;
+            GameObject newObstacle = Instantiate(obstacle[(lr + 1) / 2], new Vector3(prev_spawn_point, 0.5f, -40), this.transform.rotation);
             Obstacle obstacleScript = newObstacle.GetComponent<Obstacle>();
             obstacleScript.speed = obstacleSpeed;
             
             timer = 0.0f;
+            Star_timer++;
             lr *= -1;
+        }
+
+        if(2 * timer >= interval) {
+            if(Star_timer > Star_interval) {
+                float prob = Random.Range(0.0f, 10.0f);
+                if(prob < Star_Prob) {
+                    float star_spawn_point = Random.Range(-12.0f, 12.0f);
+                    GameObject newObstacle = Instantiate(star[0], new Vector3(prev_spawn_point * 0.5f, 0.5f, -40), this.transform.rotation);
+                    Obstacle obstacleScript = newObstacle.GetComponent<Obstacle>();
+                    obstacleScript.speed = obstacleSpeed;
+                }
+                Star_timer = 0;
+            }
         }
 
         timer += Time.deltaTime;
